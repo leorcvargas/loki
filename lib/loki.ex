d14a5cb8e5155b0362b4aeaa36ede12f38f1ab26
Leo Vargas <leocarvalho.vargas@gmail.com>
2020-12-27 21:19:06 -0300

    Add Loki.mirrow_row method

--- lib/loki.ex
+++ lib/loki.ex
@@ -9,6 +9,19 @@ defmodule Loki do
     |> hash_input
     |> build_image_with_hex_decimals
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Loki.Image{hex_decimals: hex_decimals} = _image) do
+    hex_decimals
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(fn row -> Loki.mirror_row(row) end)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def pick_color(%Loki.Image{hex_decimals: [r, g, b | _tail]} = image) do
